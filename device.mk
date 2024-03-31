7492a3d62695c41417c3286074650a605db328f4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-03-31 14:22:42 +0000

    sky: Move some performance tuning to qcom/common
    Change-Id: If04882e742f19f90e9ad895bb86fcbe84140e9ed

--- device.mk
+++ device.mk
@@ -324,9 +324,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # Perf
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 PRODUCT_ODM_PROPERTIES += \
     vendor.pasr.activemode.enabled=false \
     vendor.power.pasr.enabled=false
